08de31478fb6694f1dea742d4947d8babbf6657c
stakezone <cosmos@stake.zone>
2020-07-31 23:08:44 -0400

    test version

--- nodemonitor.sh
+++ nodemonitor.sh
@@ -9,6 +9,7 @@ validatorChecks="on"   # set to 'on' for obtaining validator metrics
 IdentityPubkey=""      # identity pubkey for validator, can be empty when it corresponds to the default keypair pair in config.yml
 voteAccount=""         # necessary in case there is more than one vote account per identity pubkey
 cli=""                 # auto detection of the solana cli can fail, in case insert like /path/solana
+rpcPort=""             # value of --rpc-port for solana-validator, insert if auto-discovery fails 
 logname=""             # a custom monitor log file name can be chosen, if left empty default is nodecheck-<username>.log
 logpath="$(pwd)"       # the directory where the log file is stored, for customization insert path like: /my/path
 logsize=200            # the max number of lines after that the log will be trimmed to reduce its size
@@ -24,12 +25,16 @@ if [ -z $IdentityPubkey ]; then IdentityPubkey=$(${installDir}/solana-keygen pub
 if [ -z $IdentityPubkey ]; then echo "please configure the IdentityPubkey in the script"; exit 1; fi
 if [ -z  $cli ]; then echo "please configure cli manually or check the configDir"; exit 1; fi
 
+if [ -z $rpcPort ]; then rpcPort="http://127.0.0.1:"$(ps aux | grep solana-validator | grep -Po "\-\-rpc\-port\s+\K[0-9]+"); fi
+if [ -z $rpcPort ]; then echo "please configure rpcPort"; exit 1; fi
+echo "$rpcPort"
+
 if [ -z $logname ]; then logname="nodemonitor-${USER}.log"; fi
 logfile="${logpath}/${logname}"
 touch $logfile
 
 echo "log file: ${logfile}"
-echo "solana bin: ${cli}"
+echo "solana cli: ${cli}"
 echo "identity pubkey: ${IdentityPubkey}"
 echo ""
 
@@ -41,32 +46,37 @@ nloglines=$(wc -l <$logfile)
 if [ $nloglines -gt $logsize ]; then sed -i "1,$(expr $nloglines - $logsize)d" $logfile; fi # the log file is trimmed for logsize
 
 date=$(date --rfc-3339=seconds)
-echo "[$date] status=scriptstarted chainid=$chainid" >>$logfile
+echo "[$date] status=scriptstarted" >>$logfile
 
 while true; do
-    if [ "$validatorChecks" == "on" ]; then
-       validatorBlockProduction=$($cli block-production | grep $IdentityPubkey | grep "$voteAccount")
-       validatorInfo="a"$($cli validators | grep $IdentityPubkey | grep "$voteAccount") #fix empty space with prefix 'a'
-       if [ $(echo $validatorInfo | awk '{print $1}') == "a!" ]; then status=delinquent; else status=active; fi
-    fi
-    validatorBlockTime=$($cli slot --commitment recent | $cli block-time)
+    validatorBlockTime=$($cli slot --commitment recent --url $rpcPort | $cli block-time --url $rpcPort)
+echo $validatorBlockTime
     validatorBlockTimeTest=$(echo $validatorBlockTime | grep -c "Date")
-    if [ -n "$validatorInfo" ] && [ "$validatorBlockTimeTest" -eq "1" ]; then
+    if [ "$validatorChecks" == "on" ]; then
+       validatorBlockProduction=$($cli block-production --url $rpcPort | grep $IdentityPubkey | grep "$voteAccount")
+       validatorInfo=$($cli validators --url $rpcPort | grep $IdentityPubkey | grep "$voteAccount") #fix empty space with prefix 'a'
+    fi
+    if [[ (-n "$validatorInfo" && "$validatorChecks" == "on")  ]] || [[ ("$validatorBlockTimeTest" -eq "1" && "$validatorChecks" != "on") ]]; then
+        validatorInfo="a"$validatorInfo  #fix empty space with prefix 'a'
+        status="up"
         blockHeight=$(echo $validatorBlockTime | grep "Block:" | awk '{print $2}')
-        blockHeightTime=$(echo $validatorBlockTime | grep "UnixTimestamp:" | awk '{print $4}' | sed 's/)/ /g')
+        blockHeightTime=$(echo $validatorBlockTime | grep "UnixTimestamp:" | awk '{print $6}' | sed 's/)/ /g')
         now=$(date --rfc-3339=seconds)
-        blockHeightFromNow=$(expr $(date +%s -d "$now") - $(date +%s -d $blockHeightTime))
-        logentry="[$now] height=${blockHeight} tFromNow=${blockHeightFromNow}"
+#        blockHeightFromNow=$(expr $(date +%s -d "$now") - $(date +%s -d $blockHeightTime))
+        blockHeightFromNow=$(expr $(date +%s) - $blockHeightTime)
+        logentry="height=${blockHeight} tFromNow=${blockHeightFromNow}"
         if [ "$validatorChecks" == "on" ]; then
-           logentry="$logentry validatorStatus=$status lastVote=$(echo $validatorInfo | awk '{print $5}') rootBlock=$(echo $validatorInfo | awk '{print $6}')"
-           if [ $status == "active" ]; then
+           if [ $(echo $validatorInfo | awk '{print $1}') == "a!" ]; then status=delinquent; elif [ "$validatorInfo" == "a" ]; then status=error; else status=validating; fi
+           logentry="$logentry lastVote=$(echo $validatorInfo | awk '{print $5}') rootBlock=$(echo $validatorInfo | awk '{print $6}')"
+           if [ $status == "validating" ]; then
               leaderSlots=$(echo $validatorBlockProduction | awk '{print $2}')
               skippedSlots=$(echo $validatorBlockProduction | awk '{print $4}')
-              pctSkipped=$(echo "scale=2 ; 100 * $skippedSlots / $leaderSlots" | bc)
+              if [ -n "$leaderSlots" ]; then pctSkipped=$(echo "scale=2 ; 100 * $skippedSlots / $leaderSlots" | bc); fi
               logentry="$logentry leaderSlots=$leaderSlots skippedSlots=$skippedSlots pctSkipped=$pctSkipped"
               logentry="$logentry credits=$(echo $validatorInfo | awk '{print $7}') activeStake=$(echo $validatorInfo | awk '{print $8}')"
            fi
         fi
+        logentry="[$now] status=$status $logentry"
         echo "$logentry" >>$logfile
     else
         now=$(date --rfc-3339=seconds)
