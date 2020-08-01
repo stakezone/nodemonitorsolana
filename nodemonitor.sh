b85470fdd21f037c5417bd89792e2b20358c0c80
stakezone <cosmos@stake.zone>
2020-08-01 12:19:46 -0400

    test version

--- nodemonitor.sh
+++ nodemonitor.sh
@@ -4,10 +4,10 @@
 
 #####    CONFIG    ##################################################################################################
 configDir="$HOME/.config/solana/" # the directory for the config files, eg.: /home/user/.config/solana/
+voteAccount=""         # vote account for the validator
 ##### optional:        #
 validatorChecks="on"   # set to 'on' for obtaining validator metrics
-IdentityPubkey=""      # identity pubkey for validator, can be empty when it corresponds to the default keypair pair in config.yml
-voteAccount=""         # necessary in case there is more than one vote account per identity pubkey
+IdentityPubkey=""      # identity pubkey for validator, insert if auto-discovery fails
 cli=""                 # auto detection of the solana cli can fail, in case insert like /path/solana
 rpcPort=""             # value of --rpc-port for solana-validator, insert if auto-discovery fails 
 logname=""             # a custom monitor log file name can be chosen, if left empty default is nodecheck-<username>.log
@@ -18,15 +18,17 @@ sleep1=30s             # polls every sleep1 sec
 
 
 if [ -z $configDir ]; then echo "please configure the config directory"; exit 1; fi
-keyfile=$(cat ${configDir}cli/config.yml | grep "keypair_path\:" | awk '{print $2}')
 installDir="$(cat ${configDir}install/config.yml | grep 'active_release_dir\:' | awk '{print $2}')/bin"
 cli="${installDir}/solana"
-if [ -z $IdentityPubkey ]; then IdentityPubkey=$(${installDir}/solana-keygen pubkey $keyfile); fi
-if [ -z $IdentityPubkey ]; then echo "please configure the IdentityPubkey in the script"; exit 1; fi
+
 if [ -z  $cli ]; then echo "please configure cli manually or check the configDir"; exit 1; fi
 
 if [ -z $rpcPort ]; then rpcPort="http://127.0.0.1:"$(ps aux | grep solana-validator | grep -Po "\-\-rpc\-port\s+\K[0-9]+"); fi
 if [ -z $rpcPort ]; then echo "please configure rpcPort"; exit 1; fi
+
+if [ -z $voteAccount ]; then echo "please configure the vote account in the script"; exit 1; fi
+if [ -z $IdentityPubkey ]; then IdentityPubkey=$(echo "a"$($cli validators --url $rpcPort | grep "$voteAccount") | awk '{print $2}'); fi #fix empty space with prefix 'a'
+if [ -z $IdentityPubkey ]; then echo "please configure the IdentityPubkey in the script"; exit 1; fi
 
 if [ -z $logname ]; then logname="nodemonitor-${USER}.log"; fi
 logfile="${logpath}/${logname}"
@@ -39,8 +41,7 @@ echo "identity pubkey: ${IdentityPubkey}"
 echo ""
 
 validatorCheck=$($cli validators)
-if [ $(grep -c $IdentityPubkey <<< $validatorCheck) == 0  ]; then echo "validator not found in set"; exit 1; fi
-if [ $(grep -c $IdentityPubkey <<< $validatorCheck) -gt 1  ]; then echo "please configure one of the vote accounts"; exit 1; fi
+if [ $(grep -c $voteAccount <<< $validatorCheck) == 0  ]; then echo "validator not found in set"; exit 1; fi
 
 nloglines=$(wc -l <$logfile)
 if [ $nloglines -gt $logsize ]; then sed -i "1,$(expr $nloglines - $logsize)d" $logfile; fi # the log file is trimmed for logsize
@@ -52,8 +53,9 @@ while true; do
     validatorBlockTime=$($cli slot --commitment recent --url $rpcPort | $cli block-time --url $rpcPort)
     validatorBlockTimeTest=$(echo $validatorBlockTime | grep -c "Date")
     if [ "$validatorChecks" == "on" ]; then
-       validatorBlockProduction=$($cli block-production --url $rpcPort | grep $IdentityPubkey | grep "$voteAccount")
-       validatorInfo=$($cli validators --url $rpcPort | grep $IdentityPubkey | grep "$voteAccount") #fix empty space with prefix 'a'
+       validatorBlockProduction=$($cli block-production --url $rpcPort | grep "$IdentityPubkey")
+echo $validatorBlockProduction
+       validatorInfo=$($cli validators --url $rpcPort | grep "$voteAccount")
     fi
     if [[ (-n "$validatorInfo" && "$validatorChecks" == "on")  ]] || [[ ("$validatorBlockTimeTest" -eq "1" && "$validatorChecks" != "on") ]]; then
         validatorInfo="a"$validatorInfo  #fix empty space with prefix 'a'
@@ -61,7 +63,7 @@ while true; do
         blockHeight=$(echo $validatorBlockTime | grep "Block:" | awk '{print $2}')
         blockHeightTime=$(echo $validatorBlockTime | grep "UnixTimestamp:" | awk '{print $6}' | sed 's/)/ /g')
         now=$(date --rfc-3339=seconds)
-#        blockHeightFromNow=$(expr $(date +%s -d "$now") - $(date +%s -d $blockHeightTime))
+        #blockHeightFromNow=$(expr $(date +%s -d "$now") - $(date +%s -d $blockHeightTime))
         blockHeightFromNow=$(expr $(date +%s) - $blockHeightTime)
         logentry="height=${blockHeight} tFromNow=${blockHeightFromNow}"
         if [ "$validatorChecks" == "on" ]; then
